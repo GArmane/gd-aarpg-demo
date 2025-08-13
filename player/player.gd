abfbcd8b298360563eebeb2a87a98e787a7f6db0
Giovanni Armane <giovanniarmane@proton.me>
2025-08-13 20:39:39 -0300

    (feat) player animations

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,17 @@
 @icon("res://assets/icon-godot-node/node-2D/icon_human_controller.png")
+
 class_name Player
 extends CharacterBody2D
 
 @export var _move_action: GUIDEAction
 @export_range(0.0, 100.0, 0.5) var _move_speed: float = 100.0
+@onready var _cardinal_direction := Vector2.DOWN
+@onready var _state := "Idle"
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	velocity = _move_action.value_axis_2d * _move_speed
+
+
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
