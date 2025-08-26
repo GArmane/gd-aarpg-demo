dc294ed4870b4c115648f1f83de75a0c3a21a5b7
Giovanni Armane <giovanniarmane@proton.me>
2025-08-26 14:44:01 -0300

    (feat) player attacking animations

--- components/player/player.gd
+++ components/player/player.gd
@@ -3,10 +3,24 @@
 class_name Player
 extends CharacterBody2D
 
+@export var _game_mode: GUIDEMappingContext
 @export var _move_action: GUIDEAction
+@export var _attack_action: GUIDEAction
+@export var _attack_sound: AudioStream
 @export_range(0.0, 100.0, 0.5) var _move_speed: float = 100.0
+@export_range(0.0, 20.0, 0.5) var _deacceleration_speed: float = 10.0
 
 var _cardinal_direction := Vector2.DOWN
+
+
+func _ready() -> void:
+	GUIDE.enable_mapping_context(_game_mode)
+
+
+func _play_audio(audio: AudioStream, pitch_scale: float = 1.0) -> void:
+	%AudioStreamPlayer2D.stream = audio
+	%AudioStreamPlayer2D.pitch_scale = pitch_scale
+	%AudioStreamPlayer2D.play()
 
 
 func _update_animation(anim: String) -> void:
@@ -21,13 +35,16 @@ func _update_animation(anim: String) -> void:
 
 
 func _on_idle_state_entered() -> void:
-	velocity = Vector2.ZERO
 	_update_animation("Movement/Idle")
 
 
-func _on_idle_state_processing(_delta: float) -> void:
+func _on_idle_state_processing(delta: float) -> void:
 	if _move_action.is_triggered() and _move_action.value_axis_2d != Vector2.ZERO:
 		%StateChart.send_event("Walking")
+	if _attack_action.is_triggered():
+		%StateChart.send_event("Attacking")
+	velocity -= velocity * _deacceleration_speed * delta
+	move_and_slide()
 
 
 func _on_walking_state_entered() -> void:
@@ -36,9 +53,11 @@ func _on_walking_state_entered() -> void:
 
 
 func _on_walking_state_processing(_delta: float) -> void:
-	# Stopped moving.
 	if not _move_action.is_triggered() or _move_action.value_axis_2d == Vector2.ZERO:
 		%StateChart.send_event("Idle")
+		return
+	if _attack_action.is_triggered():
+		%StateChart.send_event("Attacking")
 		return
 	# Changed direction sinced started moving.
 	if _cardinal_direction != _move_action.value_axis_2d:
@@ -49,4 +68,15 @@ func _on_walking_state_processing(_delta: float) -> void:
 
 
 func _on_attacking_state_entered() -> void:
-	%AnimationPlayer.play("Attack/Down")
+	_update_animation("Attack/")
+	_play_audio(_attack_sound, randf_range(0.9, 1.1))
+
+
+func _on_attacking_state_processing(delta: float) -> void:
+	velocity -= velocity * _deacceleration_speed * delta
+	move_and_slide()
+	await %AnimationPlayer.animation_finished
+	if _move_action.is_triggered():
+		%StateChart.send_event("Walking")
+	else:
+		%StateChart.send_event("Idle")
