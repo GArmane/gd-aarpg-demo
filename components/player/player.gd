690940cc4edb7a173c8a5f8401d257fbbd0a8098
Giovanni Armane <giovanniarmane@proton.me>
2025-08-27 00:34:03 -0300

    (refactor) player script uses result type

--- components/player/player.gd
+++ components/player/player.gd
@@ -17,13 +17,14 @@ func _ready() -> void:
 	GUIDE.enable_mapping_context(_game_mode)
 
 
-func _play_audio(audio: AudioStream, pitch_scale: float = 1.0) -> void:
+func _play_audio(audio: AudioStream, pitch_scale: float = 1.0) -> Result:
 	%AudioStreamPlayer2D.stream = audio
 	%AudioStreamPlayer2D.pitch_scale = pitch_scale
 	%AudioStreamPlayer2D.play()
+	return Result.ok()
 
 
-func _update_animation(anim: String) -> void:
+func _update_animation(anim: String) -> Result:
 	if _cardinal_direction.x != 0:
 		%Sprite2D.scale.x = _cardinal_direction.x
 	var anim_direction = (
@@ -32,10 +33,11 @@ func _update_animation(anim: String) -> void:
 		else "Up" if _cardinal_direction == Vector2.UP else "Side"
 	)
 	%AnimationPlayer.play(anim + anim_direction)
+	return Result.ok()
 
 
 func _on_idle_state_entered() -> void:
-	_update_animation("Movement/Idle")
+	_update_animation("Movement/Idle").unwrap()
 
 
 func _on_idle_state_processing(delta: float) -> void:
@@ -49,7 +51,7 @@ func _on_idle_state_processing(delta: float) -> void:
 
 func _on_walking_state_entered() -> void:
 	_cardinal_direction = _move_action.value_axis_2d
-	_update_animation("Movement/Walk")
+	_update_animation("Movement/Walk").unwrap()
 
 
 func _on_walking_state_processing(_delta: float) -> void:
@@ -62,14 +64,14 @@ func _on_walking_state_processing(_delta: float) -> void:
 	# Changed direction sinced started moving.
 	if _cardinal_direction != _move_action.value_axis_2d:
 		_cardinal_direction = _move_action.value_axis_2d
-		_update_animation("Movement/Walk")
+		_update_animation("Movement/Walk").unwrap()
 	velocity = _move_action.value_axis_2d.normalized() * _move_speed
 	move_and_slide()
 
 
 func _on_attacking_state_entered() -> void:
-	_update_animation("Attack/")
-	_play_audio(_attack_sound, randf_range(0.9, 1.1))
+	_update_animation("Attack/").unwrap()
+	_play_audio(_attack_sound, randf_range(0.9, 1.1)).unwrap()
 
 
 func _on_attacking_state_processing(delta: float) -> void:
