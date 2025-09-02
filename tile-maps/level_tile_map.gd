38d5c18d941c81b378ac00376299dfb20e11042c
Giovanni Armane <giovanniarmane@proton.me>
2025-09-02 17:39:40 -0300

    (refactor) remove unused variables

--- tile-maps/level_tile_map.gd
+++ tile-maps/level_tile_map.gd
@@ -2,13 +2,15 @@ class_name LevelTileMap extends TileMapLayer
 
 
 func _ready() -> void:
-	var bounds = get_tilemap_bounds()
 	GlobalLevelManager.change_tilemap_bounds(get_tilemap_bounds())
 
 
 func get_tilemap_bounds() -> TilemapLayerBounds:
 	var rect = get_used_rect()
-	return TilemapLayerBounds.new(
-		get_used_rect().position * rendering_quadrant_size,
-		get_used_rect().end * rendering_quadrant_size
+	return (
+		TilemapLayerBounds
+		. new(
+			rect.position * rendering_quadrant_size,
+			rect.end * rendering_quadrant_size,
+		)
 	)
