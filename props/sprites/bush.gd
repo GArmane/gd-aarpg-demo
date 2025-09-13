3ac3f48e11968fffa2df340e11f55e93fb2c6e45
Giovanni Armane <giovanniarmane@proton.me>
2025-09-13 01:29:37 -0300

    (fix) bushes are destroyed when hit

--- props/sprites/bush.gd
+++ props/sprites/bush.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
 
-func _on_hurtbox_damaged(_damage: int) -> void:
+func _on_hurtbox_damaged(
+	_damage: int,
+	_knockback_direction: Vector2,
+	_knockback_force: float,
+) -> void:
 	queue_free()
