a0ef9caef23cb9cbfe71f81ea5e9e14f06e6c052
Giovanni Armane <giovanniarmane@proton.me>
2026-03-27 18:17:08 -0300

    (refactor) separate items into subresources

---
+++ resources/effects/effect.gd
@@ -0,0 +1,5 @@
+@icon("res://assets/icon-godot-node/node/icon_card.png")
+class_name Effect extends Resource
+
+@export var name: String
+@export var description: String
---
+++ resources/items/consumable.gd
@@ -0,0 +1,4 @@
+@icon("res://assets/icon-godot-node/node/icon_potion.png")
+class_name Consumable extends Item
+
+@export var effects: Array[Effect] = []
