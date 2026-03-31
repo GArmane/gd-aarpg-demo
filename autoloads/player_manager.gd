62536e890fa5358b5cebe1bdc5f03ca6866c6a3b
Giovanni Armane <giovanniarmane@proton.me>
2026-03-31 03:05:41 -0300

    (docs) update docs

--- autoloads/player_manager.gd
+++ autoloads/player_manager.gd
@@ -4,6 +4,14 @@ var _player_scene = preload("res://actors/player/player.tscn")
 var _current_player: Player
 
 
+## Create a player instance.
+##
+## Returns newly created player character, overwriting previous if already present.
+func create_player_chracter() -> Player:
+	_current_player = _player_scene.instantiate()
+	return _current_player
+
+
 ## Get current instanced player.
 ##
 ## Returns the current player character. If no current player character is
@@ -13,11 +21,9 @@ func get_current_player() -> Player:
 	return _current_player
 
 
-func create_player_chracter() -> Player:
-	_current_player = _player_scene.instantiate()
-	return _current_player
-
-
+## Unparent current player character.
+##
+## Forcefully unparent current player character from scene tree.
 func unparent_player() -> Player:
 	var player := get_current_player()
 	var parent = player.get_parent()
