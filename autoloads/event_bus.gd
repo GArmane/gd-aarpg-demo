822dec9bce34a6be32028203d24814dc27046758
Giovanni Armane <giovanniarmane@proton.me>
2026-03-30 22:30:40 -0300

    (chore) add logging to GUI audio player signal

--- autoloads/event_bus.gd
+++ autoloads/event_bus.gd
@@ -8,6 +8,8 @@ signal request_play_gui_audio(stream: AudioStream)
 func _ready() -> void:
 	for p_signal in [pause, unpause]:
 		p_signal.connect(func(): _log_event(p_signal.get_name()))
+	for p_signal in [request_play_gui_audio]:
+		p_signal.connect(func(_arg): _log_event(p_signal.get_name()))
 
 
 func _log_event(event_name: String) -> void:
